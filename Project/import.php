8ad2cc119e620226619f468ec8c7291fe161b5b2
MechaCoder <talktome1224@gmail.com>
2015-01-22 21:11:42 +0000

    first commit

---
+++ Project/import.php
@@ -0,0 +1,13 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="CSS/accessibility.css">
+<link rel="stylesheet" type="text/css" href="CSS/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="CSS/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" href="CSS/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="CSS/custom-styleSheets.css">
+
+<script type="text/javascript" src="JavaScripts/bootstrap.js"></script>
+<script type="text/javascript" src="JavaScripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="JavaScripts/jquery.highlight-3.js"></script>
+<script type="text/javascript" src="JavaScripts/symbolpop.js"></script>
---
+++ Project/test.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<?php include 'import.php'; ?><!-- pls note that for text highligintins (defult in darkgray) that text mus be plced in <p></p> tags -->
+		<style id='WorkBox' type="text/css">
+			.gray-highlight:hover{background-color: rgba(0,0,0, 0.5) !important;}
+			.menu-items{text-align: center !important;}
+		</style>
+	</head>
+	<body>
+		<div class='container'>
+			<header class='row'>
+				<div class='col-md-6'>
+					<h1>TestPage</h1>
+				</div>
+				<div class='col-md-6'>
+					<div class='col-md-4 gray-highlight menu-items'>1</div>
+					<div class='col-md-4 gray-highlight menu-items'>2</div>
+					<div class='col-md-4 gray-highlight menu-items'>3</div>
+				</div>
+			</header>
+			<div class='row'>
+				<div class='col-md-8'>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat sagittis est at feugiat. Vestibulum ut ultrices diam. Mauris eu tempor lectus. Pellentesque congue, orci consectetur consequat dictum, sem lectus blandit orci, vel elementum neque nulla sit amet lectus. Integer fringilla maximus commodo. Ut eget neque at arcu lobortis dapibus ut a nisi. Integer tempus eros velit, egestas accumsan neque congue non. Quisque ac tristique metus. Nam vitae ex purus. Nam varius mi in nisi interdum ullamcorper. Aliquam accumsan diam at venenatis mattis. In id tristique enim, eget porttitor dui. Praesent mattis, lectus sed tempus aliquam, mi libero sodales neque, vel sodales sapien neque non sapien. Ut est sapien, pellentesque sit amet lobortis id, maximus et lacus.</p>
+					<p>Nulla facilisi. Cras sollicitudin lobortis elit, non vulputate mi. Praesent sed libero mollis, ultricies risus posuere, ornare libero. Maecenas id nulla et velit sollicitudin porta. Aenean tristique nisi eget convallis porta. Vestibulum iaculis blandit diam vel vestibulum. Phasellus ultricies tempus lobortis. Sed interdum leo vitae nisi aliquet volutpat. Phasellus viverra ac tortor sollicitudin blandit.</p>
+					<p>Praesent accumsan metus urna, id cursus odio iaculis non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend sapien libero, ac iaculis velit aliquet sed. Pellentesque magna metus, congue in rutrum imperdiet, ultricies ac nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse potenti. Curabitur ut eros nunc. Etiam sit amet lacus ut tellus accumsan feugiat fermentum at lorem. Sed vitae ante vel enim pellentesque elementum eget ut nisl. Pellentesque egestas facilisis nulla. Vestibulum viverra risus in magna commodo laoreet. Donec sit amet turpis non justo cursus scelerisque nec lacinia magna. Donec sit amet malesuada velit. Nam tortor lacus, ultricies sit amet est in, ultrices aliquet eros. Ut porttitor vel massa eu volutpat. Aliquam velit sapien, varius sed quam eu, convallis congue lacus.</p>
+					<p>Integer eu lectus lobortis, congue lacus ac, egestas velit. Nam sodales lorem at nibh imperdiet, vitae elementum neque sodales. Aenean eget neque nunc. Aliquam erat volutpat. Praesent efficitur purus elit, ut iaculis purus auctor et. Nam ipsum sapien, viverra sed justo non, accumsan pharetra arcu. Vestibulum nisi odio, condimentum et aliquam sed, volutpat non urna. Suspendisse potenti.</p>
+					<p>Sed tristique, sem sed fermentum vulputate, quam leo pretium risus, sed mattis diam metus vitae mi. Sed gravida malesuada est eu laoreet. Nam et est urna. Ut dignissim massa sit amet dui dictum, vitae finibus libero ultricies. Fusce aliquam mauris eu orci auctor blandit laoreet tempor lacus. Donec vitae accumsan est. Nullam a condimentum felis. Vivamus suscipit nec augue in faucibus. Suspendisse et lacus a ante sagittis porta. Nam blandit, sem placerat ullamcorper sagittis, metus tortor pretium ipsum, in pulvinar turpis eros vitae augue.</p>
+				</div>
+				<div class='col-md-4'>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat sagittis est at feugiat. Vestibulum ut ultrices diam. Mauris eu tempor lectus. Pellentesque congue, orci consectetur consequat dictum, sem lectus blandit orci, vel elementum neque nulla sit amet lectus. Integer fringilla maximus commodo. Ut eget neque at arcu lobortis dapibus ut a nisi. Integer tempus eros velit, egestas accumsan neque congue non. Quisque ac tristique metus. Nam vitae ex purus. Nam varius mi in nisi interdum ullamcorper. Aliquam accumsan diam at venenatis mattis. In id tristique enim, eget porttitor dui. Praesent mattis, lectus sed tempus aliquam, mi libero sodales neque, vel sodales sapien neque non sapien. Ut est sapien, pellentesque sit amet lobortis id, maximus et lacus.</p>
+					<p>Nulla facilisi. Cras sollicitudin lobortis elit, non vulputate mi. Praesent sed libero mollis, ultricies risus posuere, ornare libero. Maecenas id nulla et velit sollicitudin porta. Aenean tristique nisi eget convallis porta. Vestibulum iaculis blandit diam vel vestibulum. Phasellus ultricies tempus lobortis. Sed interdum leo vitae nisi aliquet volutpat. Phasellus viverra ac tortor sollicitudin blandit.</p>
+					<p>Praesent accumsan metus urna, id cursus odio iaculis non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend sapien libero, ac iaculis velit aliquet sed. Pellentesque magna metus, congue in rutrum imperdiet, ultricies ac nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse potenti. Curabitur ut eros nunc. Etiam sit amet lacus ut tellus accumsan feugiat fermentum at lorem. Sed vitae ante vel enim pellentesque elementum eget ut nisl. Pellentesque egestas facilisis nulla. Vestibulum viverra risus in magna commodo laoreet. Donec sit amet turpis non justo cursus scelerisque nec lacinia magna. Donec sit amet malesuada velit. Nam tortor lacus, ultricies sit amet est in, ultrices aliquet eros. Ut porttitor vel massa eu volutpat. Aliquam velit sapien, varius sed quam eu, convallis congue lacus.</p>
+					<p>Integer eu lectus lobortis, congue lacus ac, egestas velit. Nam sodales lorem at nibh imperdiet, vitae elementum neque sodales. Aenean eget neque nunc. Aliquam erat volutpat. Praesent efficitur purus elit, ut iaculis purus auctor et. Nam ipsum sapien, viverra sed justo non, accumsan pharetra arcu. Vestibulum nisi odio, condimentum et aliquam sed, volutpat non urna. Suspendisse potenti.</p>
+					<p>Sed tristique, sem sed fermentum vulputate, quam leo pretium risus, sed mattis diam metus vitae mi. Sed gravida malesuada est eu laoreet. Nam et est urna. Ut dignissim massa sit amet dui dictum, vitae finibus libero ultricies. Fusce aliquam mauris eu orci auctor blandit laoreet tempor lacus. Donec vitae accumsan est. Nullam a condimentum felis. Vivamus suscipit nec augue in faucibus. Suspendisse et lacus a ante sagittis porta. Nam blandit, sem placerat ullamcorper sagittis, metus tortor pretium ipsum, in pulvinar turpis eros vitae augue.</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
